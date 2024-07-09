2e39a884ffd6690bd7f5130d983c2e84b221df68
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-07-09 18:57:45 +0300

    add changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,45 +1,54 @@
+fetch('en.json')
+  .then(res => res.json())
+  .then(enTranslations => {
+    const savedLanguage = localStorage.getItem('language') || 'en';
 
-// Завантаження мовних файлів
-Promise.all([
-  fetch('en.json').then(res => res.json()),
-  fetch('uk.json').then(res => res.json())
-]).then(([enTranslations, ukTranslations]) => {
-  // Отримання збереженої мови з localStorage або встановлення за замовчуванням
-  const savedLanguage = localStorage.getItem('language') || 'en';
-
-  // Ініціалізація i18next з мовними ресурсами
-  i18next.init({
-      lng: savedLanguage, // Мова за замовчуванням
-      debug: true,
-      resources: {
+    i18next.init(
+      {
+        lng: 'en',
+        debug: true,
+        resources: {
           en: {
-              translation: enTranslations
+            translation: enTranslations,
           },
-          uk: {
-              translation: ukTranslations
-          }
-      }
-  }, function(err, t) {
-      // Ініціалізація завершена, оновлюємо текстові елементи
-      updateContent();
-  });
-});
+        },
+      },
+      function (err, t) {
+        updateContent();
 
-// Функція для зміни мови
-function changeLanguage(lng) {
-  i18next.changeLanguage(lng, function(err, t) {
-      // Зберігаємо обрану мову в localStorage
-      localStorage.setItem('language', lng);
-      updateContent();
+        if (savedLanguage !== 'en') {
+          fetch(`${savedLanguage}.json`)
+            .then(res => res.json())
+            .then(translations => {
+              i18next.addResourceBundle(
+                savedLanguage,
+                'translation',
+                translations
+              );
+              i18next.changeLanguage(savedLanguage, updateContent);
+            });
+        }
+      }
+    );
   });
+
+function changeLanguage(lng) {
+  fetch(`${lng}.json`)
+    .then(res => res.json())
+    .then(translations => {
+      i18next.addResourceBundle(lng, 'translation', translations);
+      i18next.changeLanguage(lng, function (err, t) {
+        localStorage.setItem('language', lng);
+        updateContent();
+      });
+    });
 }
 
-// Функція для оновлення текстових елементів на сторінці
 function updateContent() {
   const elements = document.querySelectorAll('[id]');
   elements.forEach(element => {
-      const key = element.id;
-      element.innerHTML = i18next.t(key);
+    const key = element.id;
+    element.innerHTML = i18next.t(key);
   });
 }
 
